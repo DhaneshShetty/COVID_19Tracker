74d1cee3e79260e223f8f93abd352ffe8c5731eb
DhaneshShetty <59681444+DhaneshShetty@users.noreply.github.com>
2020-06-27 13:11:48 +0530

    Changed design

--- app/src/main/java/com/example/covid_19tracker/Launcher.java
+++ app/src/main/java/com/example/covid_19tracker/Launcher.java
@@ -1,9 +1,14 @@
 package com.example.covid_19tracker;
 
+import android.animation.Animator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewAnimationUtils;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.covid_19tracker.Fragments.MainActivity;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -14,10 +19,17 @@ public class Launcher extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+        final View view=findViewById(R.id.launch);
+        final int x=view.getRight();
+        final int y=view.getBottom();
+        final int endRadius = (int) Math.hypot(view.getWidth(), view.getHeight());
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
-                startActivity(new Intent(Launcher.this, MainActivity.class));
+
+                        startActivity(new Intent(Launcher.this, MainActivity.class));
+
+
             }
         },4000);
     }
